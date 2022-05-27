db1f9e4908ff2b474d0b96a310a6b69f6476e368
thoushif7 <thoushif7@gmail.com>
2022-05-27 13:20:52 +0530

    added methods

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Sample {
+private void method1() {
+	System.out.println("1");
+	
+
+}
+private void method2() {
+	System.out.println("2");
+
+}
+private void method3() {
+	System.out.println("3");
+
+}
+}
